782a476f49a259a5ee7fd5dd047cabb9d99a9846
souvik1232 <souvik199831@gmail.com>
2021-01-07 15:32:35 +0530

    Refractored:Wage calculated for given duration

--- utility.js
+++ utility.js
@@ -4,6 +4,8 @@ const PART_TIME_HOUR = 4;
 const FULL_TIME = 1;
 const PART_TIME = 2;
 const WORKING_DAYS_PER_MONTH = 20;
+const MAX_HRS_IN_MONTH=100;
+const NUM_WORKING_DAYS=20;
 class EmployeeWageComputation {
     employeeAttendance=()=>{
         let attendance = Math.floor(Math.random() * 2);
@@ -16,25 +18,35 @@ class EmployeeWageComputation {
         }
     }
     dailyEmployeeWage=(attendance)=>{
+        let totalWage=0;
+        let totalWorkingDays=0;
+		let totalWorkhours=0;
         let wage;
         let monthlyWage;
         if (attendance==1) {
             let type = (Math.floor(Math.random() * 2)+1);
-            switch (type) {
-                case FULL_TIME:
-                    wage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-                    monthlyWage = wage * WORKING_DAYS_PER_MONTH;
-                    console.log("Employee Wage is : ",wage);
-                    console.log("Monthly Wage is : ",monthlyWage);
-                    break;
-                case PART_TIME:
-                    wage = WAGE_PER_HOUR * PART_TIME_HOUR;
-                    monthlyWage = wage * WORKING_DAYS_PER_MONTH;
-                    console.log("Employee Wage is : ",wage);
-                    console.log("Monthly Wage is : ",monthlyWage);
-                default:
-                    break;
+            while (totalWorkhours<MAX_HRS_IN_MONTH && totalWorkingDays < NUM_WORKING_DAYS) {
+                switch (type) {
+                    case FULL_TIME:
+                        wage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                        monthlyWage = wage * WORKING_DAYS_PER_MONTH;
+                        totalWorkhours = totalWorkhours+FULL_DAY_HOUR;
+                        // console.log("Employee Wage is : ",wage);
+                        // console.log("Monthly Wage is : ",monthlyWage);
+                        break;
+                    case PART_TIME:
+                        wage = WAGE_PER_HOUR * PART_TIME_HOUR;
+                        monthlyWage = wage * WORKING_DAYS_PER_MONTH;
+                        totalWorkhours = totalWorkhours + PART_TIME_HOUR;
+                        // console.log("Employee Wage is : ",wage);
+                        // console.log("Monthly Wage is : ",monthlyWage);
+                    default:
+                        break;
+                }
+                totalWorkingDays++;
             }
+            totalWage=(totalWorkhours*totalWorkingDays*WAGE_PER_HOUR);
+		    console.log("Total Wage is : ",totalWage);
         }
     }
 }
