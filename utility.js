7ea2716c6f476660c2eb7ffbcbd5ed0c118664dc
souvik1232 <souvik199831@gmail.com>
2021-01-07 15:16:30 +0530

    Refractored:Modified to get monthly wages

--- utility.js
+++ utility.js
@@ -3,6 +3,7 @@ const FULL_DAY_HOUR = 8;
 const PART_TIME_HOUR = 4;
 const FULL_TIME = 1;
 const PART_TIME = 2;
+const WORKING_DAYS_PER_MONTH = 20;
 class EmployeeWageComputation {
     employeeAttendance=()=>{
         let attendance = Math.floor(Math.random() * 2);
@@ -16,16 +17,21 @@ class EmployeeWageComputation {
     }
     dailyEmployeeWage=(attendance)=>{
         let wage;
+        let monthlyWage;
         if (attendance==1) {
             let type = (Math.floor(Math.random() * 2)+1);
             switch (type) {
                 case FULL_TIME:
                     wage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                    monthlyWage = wage * WORKING_DAYS_PER_MONTH;
                     console.log("Employee Wage is : ",wage);
+                    console.log("Monthly Wage is : ",monthlyWage);
                     break;
                 case PART_TIME:
                     wage = WAGE_PER_HOUR * PART_TIME_HOUR;
+                    monthlyWage = wage * WORKING_DAYS_PER_MONTH;
                     console.log("Employee Wage is : ",wage);
+                    console.log("Monthly Wage is : ",monthlyWage);
                 default:
                     break;
             }
