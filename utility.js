27b0bc4274792222466fe19a97436d094ab45651
souvik1232 <souvik199831@gmail.com>
2021-01-07 15:03:15 +0530

    Refractored:Part time Wage Calculation

--- utility.js
+++ utility.js
@@ -1,5 +1,8 @@
 const WAGE_PER_HOUR = 20;
 const FULL_DAY_HOUR = 8;
+const PART_TIME_HOUR = 4;
+const FULL_TIME = 1;
+const PART_TIME = 2;
 class EmployeeWageComputation {
     employeeAttendance=()=>{
         let attendance = Math.floor(Math.random() * 2);
@@ -14,8 +17,14 @@ class EmployeeWageComputation {
     dailyEmployeeWage=(attendance)=>{
         let wage;
         if (attendance==1) {
-            wage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-            console.log("Employee Wage is : ",wage);
+            let type = (Math.floor(Math.random() * 2)+1);
+            if (type == FULL_TIME) {
+                wage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                console.log("Employee Wage is : ",wage);
+            } else {
+                wage = WAGE_PER_HOUR * PART_TIME_HOUR;
+                console.log("Employee Wage is : ",wage);
+            }
         }
     }
 }
